123f40c12d5679c96b5b764401b218f566ff60c3
“apazdzio <anna.pazdzioch@aviva.pl>
2017-10-19 20:02:51 +0200

    15.2Commit

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -11,6 +11,5 @@ import java.util.Optional;
 public interface TaskRepository extends CrudRepository<Task, Long>{
     @Override
     List<Task> findAll();
-    @Override
-    Optional<Task> findById(Long id);
+
 }
