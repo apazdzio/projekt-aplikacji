7897546bf91cd32ae113f9f55d11e59ad5336bd2
“apazdzio <anna.pazdzioch@aviva.pl>
2017-10-24 21:32:08 +0200

    15 Commit

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -2,7 +2,6 @@ package com.crud.tasks.repository;
 
 import com.crud.tasks.domain.Task;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
